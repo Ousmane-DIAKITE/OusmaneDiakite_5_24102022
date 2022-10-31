19975a19c21d357b2f84c3c90de051e81dd83ea0
Ousmane <ousmane98@live.fr>
2022-10-31 18:03:33 +0100

    modf-chemin-script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 "use strict";
 
+
 async function loading() {
+    console.log("apiaddress = ", apiaddress);
     fetch(apiaddress)
     .then((res) => {
-    if (res.status == 200) {
-        var data = response.json()
-        console.log(data);/* Le status-code 200 de la rÃ©ponse (res) correspond Ã  une requÃªte correctement exÃ©cutÃ©e (c'est ce qu'on attend ici) */
+    console.log(res);
+    if (res.status === 200) {
+    var data = response.json()
+    console.log(data);
+    /* Le status-code 200 de la rÃ©ponse (res) correspond Ã  une requÃªte correctement exÃ©cutÃ©e (c'est ce qu'on attend ici) */
     return res.json(); /* RÃ©cupÃ©ration des articles depuis l'API au format json */
     }
     })
@@ -18,52 +22,6 @@ async function loading() {
     .catch(function (err) { return err });
     }
 
+    const apiaddress = "http://localhost:3000/api/products/";
 
-/*
-var link = document.getElementsByClassName("items").items.childNodes[1];
-var kanap = async (url) => {
-    var response = await fetch(url)
-    var data = await response.json()
-    console.log(data);
-
-}
-*/
-const apiaddress = "http://localhost:3000/api/products/";
-/*
-kanap("http://localhost:3000/api/products");
-*/
-
-function afficherProduit(article) {
-const kanap = Object[0];
-const imageElement = document.createElement("img");
-imageElement.src = "http://localhost:3000/images/kanap01.jpeg";
-const nomElement = document.createElement("h3");
-nomElement.innerText = "kanap SinopÃ©";
-const prixElement = document.createElement("p");
-prixElement.innerText = 1849;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = categorie.kanap;
-
-const section = document.querySelector("items");
-section.appendChild(imageElement);
-section.appendChild(nomElement);
-section.appendChild(prixElement);
-section.appendChild(categorieElement);
-
-}
-
-
-
-
-
-
-/*function afficherProduit() {
-    var section = document.getElementById("items"); 
-    var a = document.createElement('a');
-    var article = document.createElement('article');
-    var h3 = document.createElement('h3');
-    var p = document.createElement('p');
-    var img = document.createElement('img');
-console.log(section);
-    
-}*/
+   
