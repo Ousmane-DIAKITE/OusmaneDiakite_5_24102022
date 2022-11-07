0a7921be7685b9db3bb6228600da21d659c6d6a1
Ousmane <ousmane98@live.fr>
2022-11-07 15:58:28 +0100

    add-Products-function

--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,27 @@
 "use strict";
 fetch("http://localhost:3000/api/products/")
     .then(function(res) { 
-        if (res.ok) {
+        if (res.ok === 200) {
             return res.json();
-        }
+        }/* Si le status-code 200 de la response est (res) correspond à une requête correctement exécuté (c'est ce qu'on attend ici) */
         })
-    .then(function(value) {
-       console.log(value); 
-    })
+    .then((value) => addProducts(value)) 
     .catch((err) => {
         console.log('ça marche pas' );
     });
 
- var element = document.createElement('a');
- let anchor = document.createElement('a');
- anchor.href = "http://localhost:3000/images/kanap01.jpeg";
- const itmes = document.querySelector("items")
+ function addProducts(donnees) {
+    
+    var element = document.createElement('a');
+    let anchor = document.createElement('a');
+    anchor.href = "http://localhost:3000/images/kanap01.jpeg";
+    anchor.text = "Kanap Sinopé"
+   
+    const itmes = document.querySelector("#items")
+    itmes.appendChild(anchor)
 
+
+}
 
 
     
