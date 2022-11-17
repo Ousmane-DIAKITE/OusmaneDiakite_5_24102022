6c5b2a646ead4e2123937e40ebf862a2c7fe5bbb
Ousmane <ousmane98@live.fr>
2022-11-17 17:28:15 +0100

    function-get-article

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,10 @@
 "use strict"
+
+function getArticle() {
+    fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => addProducts(data))
+    .catch((err) => {
+        console.log('Ca marche pas');
+    });
+}
