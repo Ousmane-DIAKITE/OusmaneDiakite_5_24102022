b0213c2fbe879e299b724928915480c696ad2c94
Ousmane <ousmane98@live.fr>
2022-11-17 17:15:22 +0100

    creation-du-fichier-product-js

--- front/js/script.js
+++ front/js/script.js
@@ -1,29 +1,44 @@
 "use strict";
 
-function getArticle () { 
+getArticle();
 
-    fetch("http://localhost:3000/api/products/")
-    .then(function(res) { 
-        if (res.ok === 200) {
-            return res.json();
-        }/* Si le status-code 200 de la response est (res) correspond à une requête correctement exécuté (c'est ce qu'on attend ici) */
-        })/* Récupération des articles depuis l'API au format json */
-    .then((value) => addProducts(value)) 
+function getArticle () { 
+    fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => addProducts(data)) 
     .catch((err) => {
-        console.log('ça marche pas' );
+        console.log('ça marche pas');
     });
 }
- function addProducts(donnees) {
-    for (let i = 0; i < donnees.length; i++) {
-        const canape = donnees[i];
-    }
-    // Création de l'élément "a"
-    var linkproduct = document.createElement('a');
-    linkproduct.href = "./addProducts.html,id=" + donnees[i]._id
-    document.getElementById("items").append(linkproduct);
 
-}
-
-
-
+  // Ajout de la liste des produits
+function addProducts(donnees) {
+    for (let i=0; i < donnees.length; i++) {
+    const canape = donnees[i];
     
+    // Création de l'élément "a"
+    let linkProduct = document.createElement("a");
+    linkProduct.href = "./product.html?id=" + donnees[i]._id
+    document.getElementById("items").append(linkProduct);
+  
+    // Création de l'élément "article"
+    let productArticle = document.createElement("article");
+    linkProduct.appendChild(productArticle);
+  
+    // Création de l'élément "img"
+    let productImg = document.createElement("img");
+    productArticle.appendChild(productImg);
+    productImg.src = donnees[i].imageUrl;  
+    productImg.alt = donnees[i].altTxt;
+  
+    // Création de l'élément titre "h3"
+    let productTitle = document.createElement("h3");
+    productArticle.appendChild(productTitle);
+    productTitle.textContent = donnees[i].name;
+  
+    // Création de l'élément description "p"
+    let productDescription = document.createElement("p");
+    productArticle.appendChild(productDescription);
+    productDescription.textContent = donnees[i].description
+    }
+  }   
