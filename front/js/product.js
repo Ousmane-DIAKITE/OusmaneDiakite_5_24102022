464761977fc0aef83f328ca89dc0585edd007122
Ousmane <ousmane98@live.fr>
2022-11-17 17:58:02 +0100

    searchparams-id

--- front/js/product.js
+++ front/js/product.js
@@ -7,6 +7,7 @@
 })()
 
 function getArticle() {
+    return new URL(location.href).searchParams.get('id')
     fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((data) => addProducts(data))
