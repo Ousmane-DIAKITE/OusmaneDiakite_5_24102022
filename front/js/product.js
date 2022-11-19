2b7236eb60da698b5c5621ce3c9e08691747bc7a
Ousmane <ousmane98@live.fr>
2022-11-19 12:41:01 +0100

    boucle-pour-recuperer-data-api

--- front/js/product.js
+++ front/js/product.js
@@ -7,12 +7,16 @@ function getProduct() {
     fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((data) => { 
+        ((data) => listProduct(data)) 
         console.log(data)})
     .catch((err) => {
         console.log('Ca marche pas');
     });
 }
-
+  // Ajout de la liste des produits
 function listProduct (allCanape) {
-    
+    for (let i=0; i < allCanape.length; i++) {
+        const canape = allCanape[i];
+    }
+
 }
