136ca83ccb037470d89bc69a057965003280f5f2
Ousmane <ousmane98@live.fr>
2022-11-22 17:47:36 +0100

    fetch-par-page-produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,9 @@
 "use strict"
-// récupération de la chaine de requête dans l'url
-const queryString_url_id = window.location.search;
-const urlSearchParams = new urlSearchParams(queryString_url_id);
-const Id = urlSearchParams.get('leId');
+
 getProduct();
 
 function getProduct() {  
-    fetch("http://localhost:3000/api/products")
+    fetch("http://localhost:3000/api/products/${id}")
     .then((res) => res.json())
     .then((data) => { 
         ((data) => listProduct(data)) 
@@ -17,6 +14,13 @@ function getProduct() {
 }
   // Ajout de la liste des produits
 function listProduct (allCanape) {
+
+    // récupération de la chaine de requête dans l'url
+const queryString_url_id = window.location.search;
+const urlSearchParams = new urlSearchParams(queryString_url_id);
+const id = urlSearchParams.get('id');
+console.log(id);
+
     for (let i=0; i < allCanape.length; i++) {
 
     }
