2990a586beb990c704c6542093199b5254c68a36
Ousmane <ousmane98@live.fr>
2022-12-12 11:40:02 +0100

    affichage-img-alt_

--- front/js/product.js
+++ front/js/product.js
@@ -5,7 +5,7 @@ console.log(params);
 let id = params.get("id");
 console.log(id);
 
-
+//Fonction pour récupérer les données des produits en utilisant un fetch
 function getProduct() {  
     fetch("http://localhost:3000/api/products/" + id)
     .then(response => response.json())
@@ -14,12 +14,16 @@ function getProduct() {
 }
  function displayProduct(product) {
 
-    
+    // Création de l'élément "img"
     const productImg = document.createElement("img")
     productImg.src = product.imageUrl;  
     productImg.alt = product.altTxt;
     const divItemImg = document.querySelector(".item__img");
     divItemImg.appendChild(productImg);
+
+
+
+
  }
 
 
