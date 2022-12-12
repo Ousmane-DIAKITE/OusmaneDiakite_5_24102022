b0c51bcf72d3474a3eaee57ba11f25678d3e9109
Ousmane <ousmane98@live.fr>
2022-12-12 11:43:55 +0100

    commentaire-function

--- front/js/product.js
+++ front/js/product.js
@@ -1,17 +1,20 @@
 "use strict"
 
+// Création de let pour récupérer l'URL de chaques id
 let params = (new URL(document.location)).searchParams;
 console.log(params);
 let id = params.get("id");
 console.log(id);
 
-//Fonction pour récupérer les données des produits en utilisant un fetch
+//Création de la Fonction GetProduct pour récupérer les données des produits en utilisant un fetch
 function getProduct() {  
     fetch("http://localhost:3000/api/products/" + id)
     .then(response => response.json())
     .then((product) => displayProduct(product)) 
     .catch((error) => console.log(error)) 
 }
+
+// Création de la Fonction DisplayProduct pour afficher des element html dans le DOM
  function displayProduct(product) {
 
     // Création de l'élément "img"
@@ -20,6 +23,8 @@ function getProduct() {
     productImg.alt = product.altTxt;
     const divItemImg = document.querySelector(".item__img");
     divItemImg.appendChild(productImg);
+
+
 
 
 
