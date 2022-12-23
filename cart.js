bdf1223fe63983935bb266612787af14812eb0fa
Ousmane <ousmane98@live.fr>
2022-12-23 01:01:27 +0100

    fetch-cart-js

--- cart.js
+++ cart.js
@@ -1,1 +1,8 @@
 "use strict";
+
+//---------------------------------function appel de mon API------------------------------------------------------------
+async function getProductById (idProduct){
+    let res = await fetch("http://localhost:3000/api/products/" + idProduct)
+     return res.json();
+     console.log(res);
+ }
