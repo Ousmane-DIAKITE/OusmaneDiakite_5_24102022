deeae94cdb62ae64b4e5c5027b77bfe4a3519d25
Ousmane <ousmane98@live.fr>
2022-12-23 20:54:56 +0100

    fetch-

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,17 @@
 "use strict";
 
-let ousmane = 5 + 7
-console.log(ousmane);
+// Création d'une fonction appel de mon API
+async function getProductById (idProduct){
+    let res = await fetch("http://localhost:3000/api/products/" + idProduct)
+     return res.json();
+ }
+
+// Données du Localstorage
+async function getProductById (idProduct){
+    let basket = localStorage.getItem("basket");
+    basket = JSON.parse(basket);
+    console.log(basket);
+ }
+ 
+getProductById();
+
