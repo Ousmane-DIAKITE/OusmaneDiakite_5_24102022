043e2f4831571a8b0773f601644d21b75ea984db
Ousmane <ousmane98@live.fr>
2022-12-27 10:56:36 +0100

    mentorat

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,35 +1,51 @@
 "use strict";
 
-
+  const cart = []
+  cart.forEach((item) => displayArticle(item))
+  console.log(cart);
 // Données du Localstorage
  function getBasket() {
-    let basket = localStorage.getItem("basket");
-    basket = JSON.parse(basket);
-    console.log(basket);
-    if (basket == null ) {
-        return [];
-        
+    const numberOfItem = localStorage.length;
+    console.log(numberOfItem);
+    for (let i = 0; i < numberOfItem; i++) {
+        const item = localStorage.getItem(localStorage.key(i));
+        console.log(item);
+        const itemObject = JSON.parse(item);
+        console.log(itemObject);
+        cart.push(itemObject);   
     }
-    
- }
+ } 
+
  function displayItem(item) {
     const article = makeArticle(item);
     displayArticle(article);
     console.log(basket);
     const div = makeImageDiv(item);
-    article.appendChild(div);
-    
+    article.appendChild(div); 
  }
 
  function displayArticle(article) {
-    document.querySelector("#cart__item").appendChild(article);
-    
+    document.querySelector(".cart__item").appendChild(article);
  }
 
  function makeArticle(item) {
     const article = document.createElement("article");
     article.classList.add("cart__item");
+    article.dataset.id = item.id;
+    article.dataset.colors = item.colors;
+    return article
  }
- 
- getBasket();
 
+ function makeImageDiv(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart__item__img");
+    let divImage = document.querySelector(".img");
+        productImg.src = product.imageUrl;  
+        productImg.alt = product.altTxt;
+        div.appendChild(divImage);
+    return div
+
+ }
+
+
+ getBasket();
